09626d90fff529ce2302e584aa9aa79aed926ef8
Vivienne <annabel_chen@126.com>
2020-03-15 23:31:42 +0800

    ch03 - spring transaction -- xml

---
+++ ch03-spring-transaction-xml/src/main/java/com/springlearning/service/IAccountService.java
@@ -0,0 +1,22 @@
+package com.springlearning.service;
+
+import com.springlearning.entity.Account;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IAccountService {
+    public void saveAccount(Account account);
+
+    public void deleteAccount(int id);
+
+    public void updateAccount(Account account);
+
+    public List<Account> searchAccounts(float money);
+
+    public Account searchAccountByName(String name);
+
+    public Integer serchCount(float money);
+
+    public void transfer(String sourceName, String targetName, float money);
+}
